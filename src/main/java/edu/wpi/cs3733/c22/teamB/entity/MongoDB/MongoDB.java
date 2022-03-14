999dacb67e952038c0cae82194a9c87ece31e701
hushmandesmaeili <hushmand6@gmail.com>
2022-03-14 12:42:07 -0400

    Changed MongoDB server connection

--- src/main/java/edu/wpi/cs3733/c22/teamB/entity/MongoDB/MongoDB.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/entity/MongoDB/MongoDB.java
@@ -14,12 +14,10 @@ public class MongoDB {
 
     public static MongoClient mongoClient;
     public static MongoDatabase BDBMongo;
-//    public static  LocationMongo;
 
     public static void getConnection()  {
 
-//        try {
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://admin:admin@softwareengineeringteam.hurpz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://admin:admin@softengteam.cb0wu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .serverApi(ServerApi.builder()
@@ -27,20 +25,6 @@ public class MongoDB {
                         .build())
                 .build();
         mongoClient = MongoClients.create(settings);
-
-//            ConnectionString connectionString = new ConnectionString("mongodb+srv://admin:<admin>@softwareengineeringteam.hurpz.mongodb.net/BDBMongo?retryWrites=true&w=majority");
-//            MongoClientSettings settings = MongoClientSettings.builder()
-//                    .applyConnectionString(connectionString)
-//                    .build();
-//            mongoClient = MongoClients.create(settings);
-
-
-
-//            mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        }
-//        BDBMongo = mongoClient.getDB("BDBMongo");
 
     }
 
